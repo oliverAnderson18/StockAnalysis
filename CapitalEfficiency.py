ba9499b38297c3662a60c9b4af9eeb773d7c4af3
OliverAnderson <oliverandersonllorens18@gmail.com>
2025-06-23 15:08:08 +0200

    Spread done

---
+++ CapitalEfficiency.py
@@ -0,0 +1,124 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from Asset import Asset
+from Signal import calculate_signal
+
+class CapitalEfficiency():
+    """
+    We will now create a new class which takes care of the analysis of capital efficiency.
+    """
+    def __init__(self, asset: Asset):
+        self.ticker = asset.ticker
+        self.signals = []
+        self.ROIC = 0
+        self.WACC = 0
+        self.spread = 0
+        
+    
+    def calculate_ROIC(self) -> None:
+        """
+        ROIC: Return On Invested capitals.
+        After-tax operating profit divided by invested capital;
+        indicates efficiently a company generates returns on resources.
+        """
+        tkr = yf.Ticker(self.ticker)
+        inc = tkr.income_stmt.T.iloc[:3]
+        bal = tkr.balance_sheet.T.iloc[:3]
+
+        roics = []
+        for idx in inc.index:
+            stmt = inc.loc[idx]
+            bs = bal.loc[idx]
+            
+            ebit = stmt.loc["EBIT"] # Earnings before interests and tax
+            tax_rate = stmt.loc["Tax Provision"]/stmt.loc["Pretax Income"]
+            nopat = ebit * (1-tax_rate) # Net operating profit after tax
+            
+            equity = bs.loc["Common Stock Equity"]
+            debt = bs.loc["Total Debt"]
+            
+            # Now we calculate an aproximate of all cash that is not truly invested.
+            cash = (bs.loc["Cash Cash Equivalents And Short Term Investments"] +
+                bs.loc["Other Short Term Investments"])
+            
+            invested_capital = equity + debt - cash
+            
+            roics.append(nopat / invested_capital)
+        
+        self.ROIC = sum(roics) / len(roics)
+        
+        
+    def calculate_WACC(self) -> None:
+        """
+        WACC: Weighted-Average Cost of Capital.
+        Blended after-tax cost of equity and debt; 
+        baseline return projects require for positive value creation.
+        """
+        tkr = yf.Ticker(self.ticker)
+        inc = tkr.income_stmt.T.iloc[:3]
+        bal = tkr.balance_sheet.T.iloc[:3]
+
+        mkt_cap = tkr.fast_info["marketCap"]
+
+        beta = tkr.info.get("beta", 1) 
+        """
+        This gives the beta value (how correlated risk is of the asset is with 
+        the market) and gives a fall back of 1 (assume same risk).
+        """
+        risk_free = 0.042 # Estimated value, not exact, based on 10 year treasury
+        equity_risk_premium = 0.05 # Estimated value, not exact, based on USA/Europe stocks
+        
+        cost_equity = risk_free + equity_risk_premium * beta # CAPM
+
+        waccs = []
+        for idx in inc.index:
+            stmt = inc.loc[idx]
+            bs = bal.loc[idx]
+
+            debt = bs.loc["Total Debt"]
+            total_cap = mkt_cap + debt
+            equity_weight = mkt_cap / total_cap
+            debt_weight = debt / total_cap
+
+            interest_expense = stmt.loc["Interest Expense"]
+            cost_debt = abs(interest_expense) / debt
+            tax_rate = stmt.loc["Tax Provision"] / stmt.loc["Pretax Income"]
+            cost_debt_after_tax = cost_debt * (1 - tax_rate)
+
+            waccs.append(equity_weight * cost_equity + debt_weight * cost_debt_after_tax)
+
+        self.WACC = sum(waccs) / len(waccs)
+        
+    
+    def calculate_spread(self) -> None:
+        """
+        Difference between returns earned on invested capital and capital's cost; 
+        measures value creation or destruction.
+        """
+        self.calculate_ROIC()
+        self.calculate_WACC()
+        
+        spread = self.ROIC - self.WACC
+        self.spread = spread
+        
+        
+    def signals_capital_efficiency(self) -> list:
+        """
+        We calculate all capital efficiency variables and estimate if its in a 
+        strong/moderate/neutral buy/sell.These estimations are not rigid, but are 
+        a decent estimate for a mature asset.
+        """
+        self.calculate_spread()
+        
+        self.signals = [
+             calculate_signal(self.spread,
+                              -10, -3, 3, 10)
+        ]
+        
+        return self.signals
+        
+
+asset = Asset("MSFT", [])
+ce = CapitalEfficiency(asset)
+print(ce.signals_capital_efficiency())
